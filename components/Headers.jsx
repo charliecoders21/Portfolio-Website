8b3858be4f3a34157ec17ac991c4f83ff7285afb
charlien12 <109681764+charlien12@users.noreply.github.com>
2024-05-19 22:31:46 +0530

    all frontend code

---
+++ components/Headers.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+const Headers = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Chintan Negi <span className="text-accent">...</span>
+          </h1>
+        </Link>
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire Me</Button>
+          </Link>
+        </div>
+        <div className="xl:hidden">Mobile Nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Headers;
---
+++ components/Stairtransition.jsx
@@ -0,0 +1,7 @@
+"use client";
+
+const Stairtransition = () => {
+  return <div></div>;
+};
+
+export default Stairtransition;
