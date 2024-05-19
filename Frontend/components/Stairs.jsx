07fef9b059fe38d8f73173d1743660aea4005be2
charlien12 <109681764+charlien12@users.noreply.github.com>
2024-05-19 23:18:24 +0530

    commit stairs transition part 1

---
+++ Frontend/components/Stairs.jsx
@@ -0,0 +1,17 @@
+"use client";
+const StairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+const Stairs = () => {
+  return <div>Stairs</div>;
+};
+
+export default Stairs;
--- Frontend/components/Stairtransition.jsx
+++ Frontend/components/Stairtransition.jsx
@@ -1,7 +1,20 @@
 "use client";
 
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+import Stairs from "./Stairs";
+
 const Stairtransition = () => {
-  return <div></div>;
+  const pathName = usePathname();
+  return (
+    <AnimatePresence mode="wait">
+      <div key={pathName}>
+        <div className="h-screen w-screen fixed top-0 right-0 left-0 pointer-events-none z-50 flex">
+          <Stairs />
+        </div>
+      </div>
+    </AnimatePresence>
+  );
 };
 
 export default Stairtransition;
