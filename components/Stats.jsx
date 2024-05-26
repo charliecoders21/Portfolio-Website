8b5281c642624deea4786af8a040cdc3fb2d4c98
charlien12 <109681764+charlien12@users.noreply.github.com>
2024-05-26 10:50:01 +0530

    stats part 1

---
+++ components/Stats.jsx
@@ -0,0 +1,25 @@
+"use client";
+import CountUp from "react-countup";
+const statsStuff = [
+  {
+    num: 4,
+    text: "Years of Experience",
+  },
+  {
+    num: 5,
+    text: "Projects Coompleted",
+  },
+  {
+    num: 8,
+    text: "Technology Mastered",
+  },
+  {
+    num: 100,
+    text: "Codes Commit",
+  },
+];
+const Stats = () => {
+  return <section></section>;
+};
+
+export default Stats;
