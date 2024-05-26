445ecfb55ce7bbdbd873aa042d1f953a0b9314f3
charlien12 <109681764+charlien12@users.noreply.github.com>
2024-05-26 12:41:25 +0530

    image deploy

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -20,7 +20,8 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="https://postimg.cc/zbrcYGc1"
+            // src="https://postimg.cc/zbrcYGc1"
+            src="/static/images/IMG_0434-removebg-preview-modified.png"
             priority
             quality={100}
             fill
