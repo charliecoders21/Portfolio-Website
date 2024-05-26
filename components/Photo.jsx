23d8f033e1c4fa95a18d97e2da10359d412b184c
charlien12 <109681764+charlien12@users.noreply.github.com>
2024-05-26 11:13:41 +0530

    image deploye test 2

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -20,7 +20,7 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="https://ibb.co/jrnbDMs"
+            src="/assets/IMG_0434-removebg-preview-modified.PNG"
             priority
             quality={100}
             fill
